f349bdac1afab05837bcc1ca3a4d6b9dc91447c9
Eric Conde <condeeric@live.com>
2017-05-27 14:15:07 +0800

    add reducers

---
+++ client/reducers/comments.js
@@ -0,0 +1,8 @@
+function comments(state = [], action) {
+  console.log('====================================');
+  console.log(state, action);
+  console.log('====================================');
+  return state;
+}
+
+export default comments;
---
+++ client/reducers/posts.js
@@ -0,0 +1,8 @@
+function posts(state = [], action) {
+  console.log('====================================');
+  console.log(state, action);
+  console.log('====================================');
+  return state;
+}
+
+export default posts;
