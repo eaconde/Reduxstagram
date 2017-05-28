c9d186d373cbe6bcce72aae4b58e352015aafdc1
Eric Conde <condeeric@live.com>
2017-05-28 11:34:37 +0800

    reuse single photo view in photo

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import PhotoSingle from './PhotoSingle';
 
 const Photo = React.createClass({
   render () {
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.id);    
+    const post = this.props.posts[i]
     return (
       <div className="single-photo">
-        single photo view
+        <PhotoSingle i={i} post={post} {...this.props} />
+
       </div>
     );
   }
