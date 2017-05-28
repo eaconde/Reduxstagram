96d8ddc48beebd996958af21cda3d01758c9ecec
Eric Conde <condeeric@live.com>
2017-05-28 10:26:58 +0800

    add single photo sub-component

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import PhotoSingle from './PhotoSingle';
 
 const PhotoGrid = React.createClass({
+  // NOTE: use of ES2016 spread op (...this.props)
   render() {
     return (
-      <div class="photo-grid">
-        group photo view
+      <div className="photo-grid">
+        {this.props.posts.map((post, i) => 
+          <PhotoSingle {...this.props} key={i} i={i} post={post} />)}
       </div>
     );
   }
