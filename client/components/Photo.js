e08d5dc438cd45e375236c2fbbfda62d12424135
Eric Conde <condeeric@live.com>
2017-05-28 11:49:38 +0800

    add comments

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import PhotoSingle from './PhotoSingle';
+import Comment from './Comments';
 
 const Photo = React.createClass({
   render () {
-    const i = this.props.posts.findIndex((post) => post.code === this.props.params.id);    
+    const { id } = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === id);    
     const post = this.props.posts[i]
+    const postComments = this.props.comments[id] || [];
+    console.log('====================================');
+    console.log("postComments >> ", postComments);
+    console.log('====================================');
     return (
       <div className="single-photo">
         <PhotoSingle i={i} post={post} {...this.props} />
-
+        <Comment postComments={postComments} />
       </div>
     );
   }
